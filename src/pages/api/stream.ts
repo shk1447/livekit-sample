d9f337e9df84c9479004a4675d2ec87c6e3e3ac3
shkim <sukan8822@gmail.com>
2025-10-14 13:49:09 +0900

    ttt

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -59,7 +59,7 @@ export default async function handler(
     return res.status(500).json({ code: "server:error", result: null });
   }
 
-  const livekitHost = `http://${process.env.LIVEKIT_URL}`;
+  const livekitHost = `https://${process.env.LIVEKIT_URL}`;
   const ingressService = new IngressClient(livekitHost, apiKey, apiSecret);
 
   switch (req.method) {
